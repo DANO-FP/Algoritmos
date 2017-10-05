f760416498e3ff9b3a661c989ab57e722d79f26d
Danilo Piñeyro <danilofp13@gmail.com>
2017-10-05 10:55:30 -0300

    to finish later

--- CoffeeScriptvsJS.jsx
+++ CoffeeScriptvsJS.jsx
@@ -62,4 +62,69 @@ kids = {
     }
   };
   
+//Creating Class with CS and JQuery 
+$('.account').prop class:'active'
+
+log object.class
+
+//Creating Class just with JS
+$('.account').prop({
+  classs: 'active'
+});
+
+log(object.class);
+
 //Creating Objects with CS
+name="name"
+mask="orange"
+weapon="nuckuks"
+turtle={name,mask,weapon}
+output='#{turtle.name} wears an #{turle.mask} mask. wathc out for his #{turtle.weapon}!'
+//Creating objects with JS
+var mask,name,output,turtle,weapon;
+
+name="name";
+mask="orange";
+weapon="nuckuks";
+turtle={name,mask,weapon};
+
+output='${turtle.name} wears an ${turle.mask} mask. wathc out for his ${turtle.weapon}!';
+
+//Comments in CS
+###
+asdasdasdas
+###
+//Comments in JS
+/*
+asdasdassd
+ */
+//JS something
+//CS #something
+
+//Changes in CJ with respect to JS
+//If Else 
+//JS
+if(something)
+{
+ var mod = "yes";
+}
+if(2>1)
+{
+  something="hello";
+}
+
+date = friday?suie:jill;
+//CS
+mod ="yes" if something
+
+if 2 > 1 
+mod ="hello"
+
+if happy and funny
+WeAreHappyAndFunny();
+else
+WeNot();
+
+date=if friday then sue else jill
+
+// im here http://coffeescript.org/#splats
